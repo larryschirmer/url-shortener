84107b55219e045d76b59454dd17f6a10906af81
Larry Schirmer <larryschirmer@gmail.com>
2021-11-02 22:15:22 +0000

    feat: enable redirect and update opens

--- src/routes/l/controller.ts
+++ src/routes/l/controller.ts
@@ -1,12 +1,26 @@
 import { Request, Response, NextFunction } from 'express';
-import { nanoid } from 'nanoid';
 
-import urls, { urlSchema, Url } from '@db/urls';
+import urls from '@db/urls';
 
 const controller = {
   '/': {
     get: async (req: Request, res: Response, next: NextFunction) => {
-      
+      const { slug } = req.params;
+      try {
+        // fetch
+        const shortLink = await urls.findOne({ slug });
+        if (!shortLink?._id) throw new Error('slug is not in use');
+
+        //resolution
+        const currentTime = new Date().toISOString();
+        await urls.findOneAndUpdate(
+          { slug },
+          { $push: { opens: currentTime } }
+        );
+        res.redirect(shortLink.url);
+      } catch (e) {
+        next(e);
+      }
     }
   }
 };
