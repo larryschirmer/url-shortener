2b2054da20d2718a903a05d1368dd9f7f00225a8
Larry Schirmer <larryschirmer@gmail.com>
2021-11-03 15:15:13 +0000

    feat: add cookie parser and hide salt

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
 
 import logger from '@utils/logger';
 
@@ -13,6 +14,7 @@ app.use(helmet());
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser())
 
 app.use('/', rootRouter);
 
