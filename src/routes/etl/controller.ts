d341b9677381f976e374dead23ddad912e300863
Larry Schirmer <larryschirmer@gmail.com>
2021-12-15 17:54:28 +0000

    feat: add etl to append user to old links

--- src/routes/etl/controller.ts
+++ src/routes/etl/controller.ts
@@ -1,9 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
+import { Types } from 'mongoose';
 
 import { gen } from '@utils/hash';
 import { decodeUser } from '@utils/token';
 
 import User, { TUser } from '@db/users';
+import Url, { urlSchema, TUrl } from '@db/urls';
 
 const controller = {
   '/createUser': {
@@ -40,6 +42,43 @@ const controller = {
         const hash = await gen(password);
         const newUser: TUser = { name, password: hash, isAdmin: false };
         await User.create(newUser);
+
+        //resolution
+        res.json({ success: true });
+      } catch (e) {
+        next(e);
+      }
+    }
+  },
+  '/addUserToLinks': {
+    post: async ({ body }: Request, res: Response, next: NextFunction) => {
+      const { userId, token } = body;
+      try {
+        // validation
+        if (!token) {
+          return res.status(400).json({ error: 'Not Logged In' });
+        }
+        const userName = decodeUser(token);
+        const user = (await User.findOne({ name: userName })) ?? ({} as TUser);
+        if (!user.isAdmin) {
+          return res
+            .status(400)
+            .json({ error: 'provided user or password is not correct' });
+        }
+
+        // validate that provided user exists
+        if (!userId || typeof userId !== 'string')
+          return res.status(400).json({ error: 'user is a required field' });
+        const foundUser = await User.findOne({ _id: userId });
+        if (foundUser === null) {
+          return res.status(400).json({ error: 'user does not exist' });
+        }
+
+        // update all links with the new user
+        await Url.updateMany(
+          { user: { $exists: false } },
+          { $set: { user: new Types.ObjectId(userId) } }
+        );
 
         //resolution
         res.json({ success: true });
--- src/routes/etl/router.ts
+++ src/routes/etl/router.ts
@@ -4,5 +4,6 @@ import controller from './controller';
 const auth = express.Router();
 
 auth.post('/createUser', controller['/createUser'].post);
+auth.post('/addUserToLinks', controller['/addUserToLinks'].post);
 
 export default auth;
