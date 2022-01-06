5aaea4fd47d328cf92fdfe6c2a6845edc9bf8223
Larry Schirmer <larryschirmer@gmail.com>
2022-01-06 16:10:05 +0000

    feat: enrich returned links with favorite status

--- src/routes/slug/utils/getAdminLinks.ts
+++ src/routes/slug/utils/getAdminLinks.ts
@@ -1,19 +1,28 @@
 import { TUser } from '@db/users';
 import Url, { TUrl } from '@db/urls';
 
-const getAdminLinks = async () => {
-  type PopulatedUrl = TUrl & { _id: string; user?: TUser };
+type PopulatedUrl = TUrl & { _id: string; user?: TUser };
 
+const getAdminLinks = async () => {
   let links: PopulatedUrl[] = [];
   try {
     links = await Url.find({ isListed: true })
-      .populate<PopulatedUrl[]>('user', 'isAdmin')
+      .populate<PopulatedUrl[]>('user', 'isAdmin favorites')
       .orFail()
       .then((links: PopulatedUrl[]) => {
         return links
           .filter((link: PopulatedUrl) => link?.user?.isAdmin ?? false)
-          .map(({ _id, name, slug, url, isListed, tags, opens }) => {
-            return { _id, name, slug, url, isListed, tags, opens };
+          .map(({ _id, name, slug, url, isListed, tags, opens, user }) => {
+            return {
+              _id,
+              name,
+              slug,
+              url,
+              isListed,
+              tags,
+              opens,
+              isFavorite: user?.favorites.includes(_id) ?? false
+            };
           });
       });
   } catch (e) {
--- src/routes/slug/utils/getUserLinks.ts
+++ src/routes/slug/utils/getUserLinks.ts
@@ -1,14 +1,29 @@
+import { TUser } from '@db/users';
 import Url, { TUrl } from '@db/urls';
 
+type PopulatedUrl = TUrl & { _id: string; user?: TUser };
+
 const getUserLinks = async (userId: string) => {
-  let links: TUrl[] = [];
+  let links: PopulatedUrl[] = [];
   try {
     links = await Url.find({ user: userId })
+      .populate<PopulatedUrl[]>('user', 'favorites')
       .orFail()
-      .then((links) => {
-        return links.map(({ _id, name, slug, url, isListed, tags, opens }) => {
-          return { _id, name, slug, url, isListed, tags, opens };
-        });
+      .then((links: PopulatedUrl[]) => {
+        return links.map(
+          ({ _id, name, slug, url, isListed, tags, opens, user }) => {
+            return {
+              _id,
+              name,
+              slug,
+              url,
+              isListed,
+              tags,
+              opens,
+              isFavorite: user?.favorites.includes(_id) ?? false
+            };
+          }
+        );
       });
   } catch (e) {
     links = [];
