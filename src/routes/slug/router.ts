34e96350027796e4a27b61f7e78794ded9cd2c15
Larry Schirmer <larryschirmer@gmail.com>
2022-01-08 17:05:48 +0000

    chore: remove unused route

--- src/routes/slug/router.ts
+++ src/routes/slug/router.ts
@@ -10,7 +10,6 @@ slug.post('/', authMiddle({ protect: true }), controller['/'].post);
 slug.put('/:linkId', authMiddle({ protect: true }), controller['/'].put);
 slug.delete('/:linkId', authMiddle({ protect: true }), controller['/'].delete);
 slug.put('/favorite/:linkId', authMiddle({ protect: true }), controller['/favorite'].put);
-slug.delete('/favorite/:linkId', authMiddle({ protect: true }), controller['/favorite'].put);
 slug.get('/isValid', controller['/isValid'].get);
 
 export default slug;
