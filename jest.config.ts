5882da41a37ce7affffcde635560476c801d62a0
Larry Schirmer <larryschirmer@gmail.com>
2022-02-03 20:34:54 +0000

    chore: install jest and test runner

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  clearMocks: true,
+  coverageProvider: 'v8',
+  testMatch: [
+    '**/__tests__/**/*.[jt]s?(x)',
+    '**/__tests__/**/*.test.[jt]s?(x)'
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@db/(.*)': '<rootDir>/src/db/$1',
+    '@middleware/(.*)': '<rootDir>/src/middleware/$1',
+    '@routes/(.*)': '<rootDir>/src/routes/$1',
+    '@utils/(.*)': '<rootDir>/src/utils/$1'
+  }
+};
