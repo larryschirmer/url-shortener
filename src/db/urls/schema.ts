460cb86d4806c4613815b8f4c326029126893be7
Larry Schirmer <larryschirmer@gmail.com>
2022-03-01 22:11:55 +0000

    feat: exclude properties not responsible to user

--- src/db/urls/schema.ts
+++ src/db/urls/schema.ts
@@ -2,15 +2,13 @@ import * as yup from 'yup';
 
 import { Url } from './types';
 
-const schema: yup.SchemaOf<Omit<Url, 'user'>> = yup.object({
+const schema: yup.SchemaOf<Omit<Url, 'user' | 'tags' | 'opens'>> = yup.object({
   name: yup.string().required(),
   slug: yup.string().trim().matches(/[\w-]/i).required(),
   url: yup.string().trim().url().required(),
   isListed: yup.boolean().required(),
   isFavorite: yup.boolean().default(false),
-  description: yup.string().trim(),
-  tags: yup.array().of(yup.string().required()),
-  opens: yup.array().of(yup.string().required())
+  description: yup.string().trim()
 });
 
 export default schema;
